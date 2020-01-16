c9231faa4dfaa10fb19c6569d081edbeb580ef7e
steleal <steleal@yandex.ru>
2020-01-16 16:50:04 +0400

    Added work with menu in Main.kt

--- src/processor/Main.kt
+++ src/processor/Main.kt
@@ -3,24 +3,11 @@ package processor
 import java.util.Scanner
 
 fun main() {
-    val scanner= Scanner(System.`in`)
-    val matrixA = inputMatrix(scanner)
-    val constant = scanner.nextInt()
-
-    val result = matrixA.multyTo(constant)
-    println (result)
-}
-
-fun inputMatrix(scanner: Scanner): IntMatrix {
-    val rows = scanner.nextInt()
-    val columns = scanner.nextInt()
-    val matrix = IntMatrix(rows, columns)
-
-    for (i in 1..rows) {
-        for (j in 1..columns) {
-            val value = scanner.nextInt()
-            matrix.set(i,j, value)
-        }
+    val helper = ConsoleHelper(Scanner(System.`in`))
+    val manager = MatrixManager(helper)
+    while (!manager.needExit) {
+        manager.printMenu()
+        val cmd = manager.askCmd()
+        manager.doCmd(cmd)
     }
-    return matrix
 }
