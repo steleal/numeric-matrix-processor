d9bd79cf985b900afd30028ac765d6c4155ed85e
steleal <steleal@yandex.ru>
2020-01-16 11:47:38 +0400

    Added fun inputMatrix() in Main.kt, added input/output in main

--- src/processor/Main.kt
+++ src/processor/Main.kt
@@ -1,5 +1,26 @@
 package processor
 
+import java.util.Scanner
+
 fun main() {
-    println("Hello World!")
+    val scanner= Scanner(System.`in`)
+    val matrixA = inputMatrix(scanner)
+    val matrixB = inputMatrix(scanner)
+
+    val result = matrixA.add(matrixB)
+    println (result ?: "ERROR")
+}
+
+fun inputMatrix(scanner: Scanner): IntMatrix {
+    val rows = scanner.nextInt()
+    val columns = scanner.nextInt()
+    val matrix = IntMatrix(rows, columns)
+
+    for (i in 1..rows) {
+        for (j in 1..columns) {
+            val value = scanner.nextInt()
+            matrix.set(i,j, value)
+        }
+    }
+    return matrix
 }
