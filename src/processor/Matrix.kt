5bb6741b665e6879bb87d7de7ad2b3dfe0ca0819
steleal <steleal@yandex.ru>
2020-01-17 10:46:04 +0400

    Added fun determinant and minor in Matrix.kt. Fixed code style.

--- src/processor/Matrix.kt
+++ src/processor/Matrix.kt
@@ -50,7 +50,7 @@ class Matrix(val rows: Int, val columns: Int) {
         if (this.columns != matrix.rows) return null
         val result = Matrix(this.rows, matrix.columns)
         for (i in 0..lastRow) {
-            for (j in 0..matrix.lastColumn){
+            for (j in 0..matrix.lastColumn) {
                 var dot = 0.0
                 for (k in 0..lastColumn) {
                     dot += this.data[i][k] * matrix.data[k][j]
@@ -63,15 +63,15 @@ class Matrix(val rows: Int, val columns: Int) {
 
     override fun toString(): String {
         return data
-                .map { it.map { numberFormat.format(it)}.joinToString(" ") }
+                .map { it.map { numberFormat.format(it) }.joinToString(" ") }
                 .joinToString(System.lineSeparator())
     }
 
     fun transponseMain(): Matrix {
         val result = Matrix(columns, rows)
         for (i in 0..lastRow) {
-            for (j in 0..lastColumn){
-                result.data [j][i] = data[i][j]
+            for (j in 0..lastColumn) {
+                result.data[j][i] = data[i][j]
             }
         }
         return result
@@ -80,8 +80,8 @@ class Matrix(val rows: Int, val columns: Int) {
     fun transponseSide(): Matrix {
         val result = Matrix(columns, rows)
         for (i in 0..lastRow) {
-            for (j in 0..lastColumn){
-                result.data [lastColumn-j][lastRow-i] = data[i][j]
+            for (j in 0..lastColumn) {
+                result.data[lastColumn - j][lastRow - i] = data[i][j]
             }
         }
         return result
@@ -90,8 +90,8 @@ class Matrix(val rows: Int, val columns: Int) {
     fun transponseVertical(): Matrix {
         val result = Matrix(columns, rows)
         for (i in 0..lastRow) {
-            for (j in 0..lastColumn){
-                result.data [i][lastColumn-j] = data[i][j]
+            for (j in 0..lastColumn) {
+                result.data[i][lastColumn - j] = data[i][j]
             }
         }
         return result
@@ -100,10 +100,38 @@ class Matrix(val rows: Int, val columns: Int) {
     fun transponseHorizontal(): Matrix {
         val result = Matrix(columns, rows)
         for (i in 0..lastRow) {
-            for (j in 0..lastColumn){
-                result.data [lastRow - i][j] = data[i][j]
+            for (j in 0..lastColumn) {
+                result.data[lastRow - i][j] = data[i][j]
             }
         }
         return result
+    }
+
+    fun determinant(): Double {
+        if (rows == 1) return data[0][0]
+        if (rows == 2) {
+            return data[0][0] * data[1][1] - data[0][1] * data[1][0]
+        }
+        var result = 0.0
+        for (j in 0..lastColumn) {
+            val k = if (j % 2 == 1) -1 else 1
+            result += data[0][j] * k * this.minor(0, j)
+        }
+        return result
+    }
+
+    private fun minor(ii: Int, jj: Int): Double {
+        val size = rows - 1
+        val matrix = Matrix(size,size)
+        for (i in 0..lastRow) {
+            if (i==ii) continue
+            val ni = if (i > ii) i-1 else i
+            for (j in 0..lastColumn) {
+                if ( j == jj) continue
+                val nj = if (j > jj) j-1 else j
+                matrix.data[ni][nj] = data[i][j]
+            }
+        }
+        return matrix.determinant()
     }
 }
