6619104d37c3e088816ad34e2bb9916ce21cda9f
steleal <steleal@yandex.ru>
2020-01-27 10:54:27 +0400

    Fixed remark of Dmitry Kapitun: extract copy-past to new function

--- src/processor/ConsoleHelper.kt
+++ src/processor/ConsoleHelper.kt
@@ -6,18 +6,14 @@ class ConsoleHelper(private val scanner: Scanner) {
 
     fun print(msg: String = "") = println(msg)
 
-    fun askLine(msg: String = ""): String {
-        if (msg != "") this.print(msg)
-        return scanner.nextLine()
-    }
+    fun askLine(msg: String = "") = askForInput(scanner::nextLine, msg)
 
-    fun askInt(msg: String = ""): Int {
-        if (msg != "") this.print(msg)
-        return scanner.nextInt()
-    }
+    fun askInt(msg: String = "") = askForInput(scanner::nextInt, msg)
 
-    fun askDouble(msg: String = ""): Double {
+    fun askDouble(msg: String = "") = askForInput(scanner::nextDouble, msg)
+
+    private fun <T> askForInput(getInput: () -> T, msg: String = ""): T {
         if (msg != "") this.print(msg)
-        return scanner.nextDouble()
+        return getInput()
     }
 }
